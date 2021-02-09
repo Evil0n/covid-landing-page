9094d0d3faa43bae984db430ed201f2d04cef084
Arseny <apritsepa@gmail.com>
2021-02-09 12:38:03 +0300

    Updates

---
+++ precache-manifest.2f6bdf639cb4b896f9558e977e87b538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a622b1a5511cc467bcecd6735ccf20dd",
+    "url": "/covid-landing-page/index.html"
+  },
+  {
+    "revision": "69b7968e9c27363ec3ac",
+    "url": "/covid-landing-page/static/css/main.565c40cf.chunk.css"
+  },
+  {
+    "revision": "68e57bc61e046079ff77",
+    "url": "/covid-landing-page/static/js/2.c0e2d9e0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-landing-page/static/js/2.c0e2d9e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b7968e9c27363ec3ac",
+    "url": "/covid-landing-page/static/js/main.ca573883.chunk.js"
+  },
+  {
+    "revision": "573fcdd0dad7752f8d80",
+    "url": "/covid-landing-page/static/js/runtime-main.0f550375.js"
+  }
+]);
